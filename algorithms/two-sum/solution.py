12ca2d2432dbc42c1383952a485355d673bca6b2
Ananya kushwaha <kushwahaananya4197@gmail.com>
2026-08-20 14:05:57 +0530

    feat: add Two Sum solution

--- algorithms/two-sum/solution.py
+++ algorithms/two-sum/solution.py
@@ -1,9 +1,6 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        result = 0
-
-        for num in nums:
-            result ^= num
-
-        return result
-        
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1 , len(nums)):
+                if nums[i]+nums[j] == target:
+                  return[i ,j]
