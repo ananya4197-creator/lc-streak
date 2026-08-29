ef383f9aa499e2aaa766e568c4aac9a2925a3449
Ananya kushwaha <kushwahaananya4197@gmail.com>
2026-08-29 19:34:14 +0530

    feat: add Reverse Words in a String solution

--- algorithms/reverse-words-in-a-string/solution.py
+++ algorithms/reverse-words-in-a-string/solution.py
@@ -1,17 +1,3 @@
 class Solution:
-    def reverse(self, x: int) -> int:
-        y = str(x)
-        
-        if y[0] == "-":
-            ans =   "-" + y[:0: -1]
-
-        else:
-            ans =   int(y[:: -1])
-          
-        ans = int(ans)
-
-        if ans < -2**31 or ans >2**31 :
-            return 0
-
-        return ans
-            
+    def reverseWords(self, s: str) -> str:
+        return " ".join(s.split()[::-1])
