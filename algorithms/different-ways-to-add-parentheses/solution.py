2501c46c90d2cc38566dd5cac2ccc3b8e592349f
Ananya kushwaha <kushwahaananya4197@gmail.com>
2026-09-08 23:16:30 +0530

    feat: add Different Ways to Add Parentheses solution

---
+++ algorithms/different-ways-to-add-parentheses/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def diffWaysToCompute(self, expression):
+        ans = []
+
+        for i in range(len(expression)):
+            if expression[i] in "+-*":
+                
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i + 1:])
+
+                for a in left:
+                    for b in right:
+                        if expression[i] == "+":
+                            ans.append(a + b)
+                        elif expression[i] == "-":
+                            ans.append(a - b)
+                        else:
+                            ans.append(a * b)
+
+        if not ans:
+            ans.append(int(expression))
+
+        return ans
