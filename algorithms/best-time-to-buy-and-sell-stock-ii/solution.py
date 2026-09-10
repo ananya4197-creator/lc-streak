cd3ae683bee1b4c458563474e89c0e831a7ef239
Ananya kushwaha <kushwahaananya4197@gmail.com>
2026-09-10 23:44:15 +0530

    feat: add Best Time to Buy and Sell Stock II solution

--- algorithms/best-time-to-buy-and-sell-stock-ii/solution.py
+++ algorithms/best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,19 +1,9 @@
 class Solution:
-    def countAndSay(self, n: int) -> str:
-        result = "1"
+    def maxProfit(self, prices):
+        profit = 0
 
-        for _ in range(n - 1):
-            current = ""
-            count = 1
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
 
-            for i in range(1, len(result)):
-                if result[i] == result[i - 1]:
-                    count += 1
-                else:
-                    current += str(count) + result[i - 1]
-                    count = 1
-
-            current += str(count) + result[-1]
-            result = current
-
-        return result
+        return profit
