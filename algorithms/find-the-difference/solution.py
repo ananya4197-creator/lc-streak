2cc9d34c403b2b480ffb40dae4963cfb780e6c5c
Ananya kushwaha <kushwahaananya4197@gmail.com>
2026-09-17 22:41:36 +0530

    feat: add Find the Difference solution

--- algorithms/find-the-difference/solution.py
+++ algorithms/find-the-difference/solution.py
@@ -1,10 +1,5 @@
-from collections import Counter
-
 class Solution:
-    def findKthLargest(self, nums: list[int], k: int) -> int:
-        a = Counter(nums)
-        
-        for num in sorted(a.keys(), reverse=True):
-            if k <= a[num]:
-                return num
-            k -= a[num]
+    def findTheDifference(self, s: str, t: str) -> str:
+        for ch in t:
+            if t.count(ch) > s.count(ch):
+                return ch
