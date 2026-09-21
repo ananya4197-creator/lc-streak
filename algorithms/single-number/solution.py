db25a3d4d25711f84e05af551b96efc769c54e7a
Ananya kushwaha <kushwahaananya4197@gmail.com>
2026-09-21 23:20:30 +0530

    feat: add Single Number solution

--- algorithms/single-number/solution.py
+++ algorithms/single-number/solution.py
@@ -1,16 +1,9 @@
 class Solution:
-    def maxProduct(self, nums):
-        curr_max = nums[0]
-        curr_min = nums[0]
-        ans = nums[0]
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
 
-        for i in range(1, len(nums)):
-            if nums[i] < 0:
-                curr_max, curr_min = curr_min, curr_max
+        for num in nums:
+            result ^= num
 
-            curr_max = max(nums[i], curr_max * nums[i])
-            curr_min = min(nums[i], curr_min * nums[i])
-
-            ans = max(ans, curr_max)
-
-        return ans
+        return result
+        
